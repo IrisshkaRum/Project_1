0c560db25c2efefaa64898c489d4f59510b265a8
IrisshkaRum <vanceroff@gmail.com>
2024-11-24 23:07:41 +0300

    Update config.py

---
+++ tic-tac-toe_package/config.py
@@ -0,0 +1,12 @@
+'''В этом файле содержатся некоторые константы'''
+WIDTH, HEIGHT = 600, 600
+LINE_WIDTH = 15
+BOARD_ROWS, BOARD_COLS = 3, 3
+SQUARE_SIZE = WIDTH // BOARD_COLS
+
+# Цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
